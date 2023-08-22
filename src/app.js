746cfc16dbdb155d884659e253dc6869b8f677fd
Minh452003 <117347792+Minh452003@users.noreply.github.com>
2023-08-22 20:28:31 +0700

    feat: create getAll, get. delete products

--- src/app.js
+++ src/app.js
@@ -2,11 +2,13 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv"
+import routerProducts from "./routers/products.js";
 
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use("/api", routerProducts);
 
 
 
