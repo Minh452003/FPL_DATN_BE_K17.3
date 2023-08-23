85c60417c2eadb72fe463b63c21ac97b2024d5a7
Your Name <chubachduong582003@gmail.com>
2023-08-23 19:55:33 +0700

    feat/create signin signup

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,5 +1,8 @@
 import user from "../models/user.js";
-
+import bcrypt from "bcryptjs";
+import User from "../models/user.js";
+import { signinSchema, signupSchema } from "../schemas/auth.js";
+import jwt from "jsonwebtoken";
 
 export const getAll = async (req, res) => {
     try {
@@ -42,5 +45,87 @@ export const remove = async (req, res) => {
         return res.status(400).json({
             message: error,
         })
+    }
+};
+
+export const signup = async (req, res) => {
+    try {
+        const { first_name, last_name,email,phone,address,avatar, password } = req.body;
+        const { error } = signupSchema.validate(req.body, { abortEarly: false });
+        if (error) {
+            const errors = error.details.map((err) => err.message);
+            return res.status(400).json({
+                message: errors,
+            });
+        }
+        const userExist = await User.findOne({ email });
+        if (userExist) {
+            return res.status(400).json({
+                messsage: "Email đã tồn tại",
+            });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const user = await User.create({
+            first_name,
+            last_name,
+            phone,
+            address,
+            avatar,
+            email,
+            password: hashedPassword,
+        });
+
+        // Không trả password
+        user.password = undefined;
+
+        return res.status(201).json({
+            message: "Đăng ký thành công",
+            user,
+        });
+    } catch (error) {
+        return res.status(400).json({
+            message: error,
+        });
+    }
+};
+
+
+export const signin = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const { error } = signinSchema.validate(req.body, { abortEarly: false });
+        if (error) {
+            const errors = error.details.map((err) => err.message);
+            return res.status(400).json({
+                messages: errors,
+            });
+        }
+
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(404).json({
+                message: "Tài khoản không tồn tại",
+            });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({
+                message: "Mật khẩu không đúng",
+            });
+        }
+        const token = jwt.sign({ id: user._id }, "DATN", { expiresIn: "1d" });
+        return res.status(200).json({
+            message: "Đăng nhập thành công",
+            accessToken: token,
+            user,
+        });
+    } catch (error) {
+        return res.status(400).json({
+            message: error,
+        });
     }
 };
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getAll, getOneById, remove } from "../controllers/auth.js";
+import { getAll, getOneById, remove, signin, signup } from "../controllers/auth.js";
 
 
 const routerUser = express.Router();
@@ -7,5 +7,6 @@ const routerUser = express.Router();
 routerUser.get("/users", getAll);
 routerUser.get("/users/:id", getOneById);
 routerUser.delete("/users/:id", remove);
-
+routerUser.post("/signup", signup);
+routerUser.post("/signin", signin);
 export default routerUser
