1a44ab448e60d3eb28e3625c9c4f52c0cc8ba45d
tranquyluong <105479930+tranquyluong@users.noreply.github.com>
2023-08-24 02:05:08 +0700

    feact : [getAllCategory,getCategoryById,RemoveCategory]

--- src/routers/category.js
+++ src/routers/category.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { addCategory, updateCategory } from "../controllers/category.js";
+import { RemoveCategory, addCategory, getAllCategory, getCategoryById, updateCategory } from "../controllers/category.js";
 
 const routerCategory = express.Router();
 
 routerCategory.post("/category", addCategory)
+routerCategory.get("/category/:id", getCategoryById)
+routerCategory.delete("/category/:id", RemoveCategory)
+routerCategory.get("/category", getAllCategory)
 routerCategory.patch("/category/:id", updateCategory)
+
 
 export default routerCategory;
