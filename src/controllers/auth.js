7f2fad34e78508595f7dda02618f7ed9ec4d5d7f
Duyy2707 <duy27072003@gmail.com>
2023-08-24 18:55:54 +0700

    fix: message remove Auth

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -20,13 +20,13 @@ export const getOneById = async (req, res) => {
         const data = await user.findById(id);
         if (data === 0) {
             return res.status(400).json({
-                message: "Hiện tt người dùng thất bại",
+                message: "Hiện thông tin người dùng thất bại",
             })
         }
         const { _id, first_name,last_name,password, email, address, phone, role, avatar, createdAt } = data;
 
         return res.status(200).json({
-            _id, first_name,last_name,password, email, address, phone, role, avatar, createdAt
+            _id, first_name,last_name,password, email, address, phone, role, avatar, createdAt        
         });
     } catch (error) {
         return res.status(400).json({
@@ -39,7 +39,7 @@ export const remove = async (req, res) => {
         const id = req.params.id;
         const data = await user.findByIdAndDelete(id);
         return res.status(200).json({
-            message: "Xoá sản phẩm thành công",
+            message: "Xóa thông tin người dùng thành công",
         })
     } catch (error) {
         return res.status(400).json({
