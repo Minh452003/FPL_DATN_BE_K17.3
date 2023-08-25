09dce232704d15e18459988899883215a945944f
Your Name <chubachduong582003@gmail.com>
2023-08-25 20:17:06 +0700

    feat: create model schema order

---
+++ src/models/orders.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+
+const orderSchema = mongoose.Schema({
+  userId: {
+    type: mongoose.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+  couponld : {
+    type: mongoose.Types.ObjectId,
+    ref: "Couponld",
+    require : true
+  }
+  ,
+  products: [
+    {
+        productId: { type: mongoose.Schema.Types.ObjectId, required: true },
+        product_name: String,
+        product_price: Number,
+        image: String,
+        stock_quantity: Number,
+    }
+  ],
+  total:{
+    type:Number,
+    require:true
+  },
+  status : {
+    type: mongoose.Types.ObjectId,
+    ref: "Status",
+    default :'64e8a93da63d2db5e8d8562a'
+  },
+  phone: {
+    type: String,
+    required: true
+  },
+  address:{
+    type:String,
+    require :true
+  },
+  notes:{
+    type:String
+  }
+},
+{ timestamps: true, versionKey: false });
+export default mongoose.model("order", orderSchema);
---
+++ src/schemas/order.js
@@ -0,0 +1,30 @@
+import Joi from "joi";
+export const orderSchema = Joi.object({
+    _id: Joi.string(),
+    userId: Joi.string().required().messages({
+        "string.empty": "ID người dùng bắt buộc nhập",
+        "any.required": "Trường ID người dùng bắt buộc nhập",
+        "string.base": "ID người dùng phải là string"
+    }),
+    couponld: Joi.string().required().messages({
+        "string.empty": "ID sản phẩm bắt buộc nhập",
+        "any.required": "Trường ID sản phẩm bắt buộc nhập",
+        "string.base": "ID sản phẩm phải là string"
+    }),
+    products: Joi.array().required(),
+    total: Joi.number().required().min(0),
+    status: Joi.string().required().message({
+        "string.empty": "mời nhập trạng thái",
+        "any.required": "Phải nhập trạng thái",
+    }),
+    phone: Joi.string().max(12).required().messages({
+        "string.empty": "Mời điền số điện thoại",
+        "any.required": "bắt buộc thêm số điện thoại",
+        "string.max": "Số phải phải có ít hơn 12 số",
+    }),
+    address: Joi.string().required().messages({
+        "string.empty": "Thêm địa chỉ ",
+        "any.required": 'Trường "Địa chỉ" là bắt buộc',
+    }),
+    notes: Joi.string()
+})
