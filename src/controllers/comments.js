496e314e3bcac7ee85324815394ce6154dedb7e1
tranquyluong <105479930+tranquyluong@users.noreply.github.com>
2023-08-30 00:06:40 +0700

    [BE] fix: comment-review

--- src/controllers/comments.js
+++ src/controllers/comments.js
@@ -1,7 +1,8 @@
 
 import Comment from "../models/comments.js";
 import { CommentSchema } from "../schemas/comments.js";
-
+import Product from "../models/products.js";
+import User from "../models/user.js";
 
 export const getCommentFromProduct = async (req, res) => {
     const { productId } = req.params;
@@ -59,9 +60,8 @@ export const getOneComment = async (req, res) => {
     }
 }
 
-
 export const create = async (req, res) => {
-
+    const { userId, rating, description, productId } = req.body;
     try {
         const { error } = CommentSchema.validate(req.body, { abortEarly: false });
         if (error) {
@@ -70,11 +70,65 @@ export const create = async (req, res) => {
                 message: errors
             })
         }
-        const comment = await Comment.create(req.body);
-        return res.status(200).json({
-            message: "Tạo comment thành công",
-            comment,
-        });
+        if (!userId) {
+            return res.status(401).json({
+              message: "Bạn phải đang nhập mới được đánh giá sản phẩm!",
+            });
+        }
+         // Check if the product exists
+        const product = await Product.findById(productId);
+
+        if (!product) {
+             return res.status(404).json({
+                message: "Sản phẩm không tồn tại.",
+             });
+        }
+          // Check if the user exists
+        const user = await User.findById(userId);
+
+          if (!user) {
+          return res.status(404).json({
+          message: "Người dùng không tồn tại.",
+          });
+        }
+
+          // Check if the user already reviewed the product
+    const existingComment = await Comment.findOne({ userId, productId });
+
+        if (existingComment) {
+          return res.status(401).json({
+          message: "Bạn đã đánh giá sản phẩm này trước đó.",
+         });
+        }
+        const user_fullName = user?.user_fullName;
+        const user_avatar = user?.user_avatar;
+        const comment = await Comment.create({
+        user_fullName,
+        user_avatar,
+        userId,
+        rating,
+        description,
+        productId,
+    });
+       const comments = await Comment.find({ productId });
+        const totalRating = comments.reduce(
+         (totalRating, rating) => totalRating + rating.rating,
+        0
+      );
+     // Tính toán số lượng sao và lươtj đánh giá
+        const reviewCount = comments.length;
+         const averageScore = totalRating / reviewCount;
+ 
+         product.average_score = Math.round(averageScore);
+         product.review_count = reviewCount;
+        await product.save();
+     if (user) {
+       return res.status(200).json({
+         message: "Bạn đã đánh giá thành công sản phẩm này!",
+         success: true,
+         comment,
+       });
+     }
     } catch (error) {
         res.status(400).json({
             message: error,
