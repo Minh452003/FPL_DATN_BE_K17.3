3c9d5341dda63c00842b3c5009ff81473a903a90
Tran Hai Duong <haiduong12122003@gmail.com>
2023-08-29 11:02:15 +0700

    fix : createOrder - updateOrder

--- src/controllers/order.js
+++ src/controllers/order.js
@@ -1,29 +1,11 @@
 import Order from "../models/orders.js"
+import { orderSchema } from "../schemas/order.js";
 
-export const createOrder = async (req, res) => {
-    try {
-        const order = await Order.create(req.body)
-        if (!order) {
-            return res.status(404).json({
-                error: "Đặt hàng thất bại"
-            })
-        }
-        return res.status(200).json({
-            message: "Đặt hàng thành công",
-            order
-        })
-    } catch (error) {
-        return res.status(400).json({
-            message: error.message
-        })
-
-    }
-}
 
 export const getOrderByUserId = async (req, res) => {
     try {
         const id = req.params.userId
-        const order = await Order.find({ id }).select('-products.productId -status');
+        const order = await Order.find({ id }).populate('-products.productId -status');
         return res.status(200).json({
             message: "Lấy thông tin người dùng đặt hàng thành công",
             order
@@ -55,7 +37,6 @@ export const getOrderById = async (req, res) => {
     }
 }
 
-
 export const getAllOrder = async (req, res) => {
     try {
         const order = await Order.find().populate('products.productId status');
@@ -75,7 +56,6 @@ export const getAllOrder = async (req, res) => {
     }
 }
 
-
 export const removeOrder = async (req, res) => {
     try {
         const removeOrder = await Order.findByIdAndDelete(req.params.id);
@@ -94,12 +74,44 @@ export const removeOrder = async (req, res) => {
     }
 }
 
+export const createOrder = async (req, res) => {
+    try {
+        const body = req.body;
+        const { error } = orderSchema.validate(body, { abortEarly: false })
+        if (error) {
+            return res.status(400).json({
+                message: error.details[0].message
+            })
+        }
+        const order = await Order.create(body)
+        if (!order) {
+            return res.status(404).json({
+                error: "Đặt hàng thất bại"
+            })
+        }
+        return res.status(200).json({
+            message: "Đặt hàng thành công",
+            order
+        })
+    } catch (error) {
+        return res.status(400).json({
+            message: error.message
+        })
+
+    }
+}
 
 export const updateOrder = async (req, res) => {
-    const id = req.params.id;
-    const { statusId } = req.body
     try {
-        const updateOrder = await Order.findByIdAndUpdate(id, { status: statusId }, { new: true }).populate('products.productId status')
+        const id = req.params.id;
+        const body = req.body;
+        const { error } = orderSchema.validate(body, { abortEarly: false })
+        if (error) {
+            return res.status(400).json({
+                message: error.details[0].message
+            })
+        }
+        const updateOrder = await Order.findByIdAndUpdate(id, body, { new: true }).populate('products.productId status')
         if (!updateOrder) {
             return res.status(404).json({
                 message: "Đơn hàng không tồn tại"
@@ -107,7 +119,7 @@ export const updateOrder = async (req, res) => {
         }
         return res.status(200).json({
             message: "Cập nhật đơn hàng thành công",
-            data: updateOrder
+            orderUpdateSuccess: updateOrder
         })
     } catch (error) {
         return res.status(400).json({
