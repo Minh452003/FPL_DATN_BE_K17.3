6268a6b8a4826dd6e4bc79dea786f31c1425cd74
Your Name <chubachduong582003@gmail.com>
2023-08-31 17:28:20 +0700

    feat: Soft Delete Category

--- src/routers/category.js
+++ src/routers/category.js
@@ -1,14 +1,17 @@
 import express from "express";
-import { addCategory, getAllCategory, getCategoryById, removeCategory, updateCategory } from "../controllers/category.js";
+import { addCategory, getAllCategory, getAllDelete, getCategoryById, removeCategory, removeForce, restoreCategory, updateCategory } from "../controllers/category.js";
 import { checkPermission } from "../middlewares/checkPermission.js";
 
 const routerCategory = express.Router();
 
-routerCategory.post("/category", checkPermission, addCategory)
-routerCategory.get("/category/:id", getCategoryById)
-routerCategory.delete("/category/:id", checkPermission, removeCategory)
 routerCategory.get("/category", getAllCategory)
-routerCategory.patch("/category/:id", checkPermission, updateCategory)
+routerCategory.get("/category/delete", getAllDelete)
+routerCategory.get("/category/:id", getCategoryById)
+routerCategory.delete("/category/:id",checkPermission, removeCategory)
+routerCategory.delete("/category/force/:id",checkPermission, removeForce)
+routerCategory.post("/category",checkPermission, addCategory)
+routerCategory.patch("/category/:id",checkPermission, updateCategory)
+routerCategory.patch("/category/restore/:id",checkPermission, restoreCategory   )
 
 
 export default routerCategory;
