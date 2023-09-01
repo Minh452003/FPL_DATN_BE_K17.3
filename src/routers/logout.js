a83a2ceafa5a0cc98c956c886688356369ae810b
nguyenloc9999 <nguyenloc31102003@gmail.com>
2023-09-01 08:57:01 +0700

    fix:create_logout

--- src/routers/logout.js
+++ src/routers/logout.js
@@ -1,11 +1,11 @@
-
 import express from 'express';
 import { logout } from '../controllers/logout.js';
 
 const routerLogout = express.Router();
 
-// Đường dẫn đăng xuất
-routerLogout.post('/logout', logout);
+routerLogout.post("/logout", logout);
 
 export default routerLogout;
+
+
 
