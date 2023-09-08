ce1a3265e0e7f8138c6af63c1c7669f9c8654d35
Minh452003 <117347792+Minh452003@users.noreply.github.com>
2023-09-08 19:45:58 +0700

    feat: create payment momo

--- src/controllers/payments.js
+++ src/controllers/payments.js
@@ -6,13 +6,12 @@ export const PayMomo = (req, res) => {
     const secretKey = 'K951B6PE1waDMi640xX08PD3vg6EkVlz';
     const orderInfo = 'THANH TOÁN MOMO';
     const partnerCode = 'MOMO';
-    const redirectUrl = 'https://webhook.site/b3088a6a-2d17-4f8d-a383-71389a6c600b';
+    const redirectUrl = 'http://localhost:5173/thanks';
     const ipnUrl = 'http://localhost:8088/api/thanks';
     const requestType = "payWithMethod";
     const amount = req.body.total;
     const orderId = partnerCode + new Date().getTime();
     const requestId = orderId;
-    const extraData = '';
     // Bổ sung
     const userId = req.body.userId;
     const couponId = req.body.couponId;
@@ -20,13 +19,13 @@ export const PayMomo = (req, res) => {
     const status = req.body.status;
     const phone = req.body.phone;
     const address = req.body.address;
-    const paymentCode = 'T8Qii53fAXyUftPV3m9ysyRhEanUs9KlOPfHgpMR0ON50U10Bh+vZdpJU7VY4z+Z2y77fJHkoDc69scwwzLuW5MzeUKTwPo3ZMaB29imm6YulqnWfTkgzqRaion+EuD7FN9wZ4aXE1+mRt0gHsU193y+yxtRgpmY7SDMU9hCKoQtYyHsfFR5FUAOAKMdw2fzQqpToei3rnaYvZuYaxolprm9+/+WIETnPUDlxCYOiw7vPeaaYQQH0BF0TxyU3zu36ODx980rJvPAgtJzH1gUrlxcSS1HQeQ9ZaVM1eOK/jl8KJm6ijOwErHGbgf/hVymUQG65rHU2MWz9U8QUjvDWA==';
+    const extraData = `userId=${userId}&couponId=${couponId}&phone=${phone}&address=${address}&products=${JSON.stringify(products)}`;
     const orderGroupId = '';
     const autoCapture = true;
     const lang = 'vi';
     // Create raw signature
     const rawSignature = `accessKey=${accessKey}&amount=${amount}&extraData=${extraData}&ipnUrl=${ipnUrl}&orderId=${orderId}&orderInfo=${orderInfo}&partnerCode=${partnerCode}&redirectUrl=${redirectUrl}&requestId=${requestId}&requestType=${requestType}`;
-
+    console.log(rawSignature);
     // Generate signature
     const signature = crypto.createHmac('sha256', secretKey)
         .update(rawSignature)
@@ -67,13 +66,11 @@ export const PayMomo = (req, res) => {
         headers: {
             'Content-Type': 'application/json',
             'Content-Length': Buffer.byteLength(requestBody)
-        }
+        },
     };
 
     // Send the request and get the response
     const momoRequest = request(options, momoResponse => {
-        console.log(`Status: ${momoResponse.statusCode}`);
-        console.log(`Headers: ${JSON.stringify(momoResponse.headers)}`);
         momoResponse.setEncoding('utf8');
         momoResponse.on('data', body => {
             res.json({ payUrl: JSON.parse(body).payUrl });
@@ -82,7 +79,6 @@ export const PayMomo = (req, res) => {
             console.log('No more data in response.');
         });
     });
-
     momoRequest.on('error', error => {
         console.log(`Problem with request: ${error.message}`);
         res.status(500).json({ error: 'Internal Server Error' });
--- src/routers/payments.js
+++ src/routers/payments.js
@@ -3,6 +3,7 @@ import { PayMomo } from '../controllers/payments.js';
 const routerPayment = express.Router();
 
 routerPayment.post("/create_payment_url", PayMomo);
+
 // routerPayment.get("/vnpay_return", CheckPay)
 export default routerPayment;
 
