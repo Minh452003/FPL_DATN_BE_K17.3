2182e4d074d5bfa99d29076611b11f88337ca49f
chubachduong <121935645+chubachduong@users.noreply.github.com>
2023-09-16 14:09:24 +0700

    Update size.js

--- src/schemas/size.js
+++ src/schemas/size.js
@@ -3,12 +3,12 @@ import Joi from "joi";
 export const SizeSchema = Joi.object({
     _id: Joi.string(),
     size_name: Joi.string().required().messages({
-        "string.empty": "Tên màu không được để trống",
-        "any.required": "Trường tên màu bắt buộc nhập"
+        "string.empty": "Tên size không được để trống",
+        "any.required": "Trường tên size bắt buộc nhập"
     }),
     size_price: Joi.number().required().messages({
-        "number.empty": "Giá màu bắt buộc nhập",
-        "any.required": "Trường giá màu bắt buộc nhập",
-        "number.base": "Giá màu phải là số"
+        "number.empty": "Giá size bắt buộc nhập",
+        "any.required": "Trường giá size bắt buộc nhập",
+        "number.base": "Giá size phải là số"
     })
 })
