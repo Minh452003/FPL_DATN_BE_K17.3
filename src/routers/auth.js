2e9577acacdbc4eadf53793605267e49a03054a6
Tran Hai Duong <haiduong12122003@gmail.com>
2023-09-22 09:22:20 +0700

    feat : Admin can update users

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getAll, getOneById, logout, refreshToken, removebyAdmin, removebyUser, signin, signup, updateUser } from "../controllers/auth.js";
+import { getAll, getOneById, logout, refreshToken, removebyAdmin, removebyUser, signin, signup, updateUser, updateUserByAdmin } from "../controllers/auth.js";
 import { authorization } from "../middlewares/authorization.js";
 import { authenticate } from "../middlewares/authenticate.js";
 
@@ -8,9 +8,10 @@ const routerAuth = express.Router();
 
 routerAuth.get("/users", getAll);
 routerAuth.get("/users/:id", getOneById);
-routerAuth.delete("/users/:id", authenticate, authorization, removebyAdmin);
-routerAuth.delete("/user/:id", authenticate, authorization, removebyUser);
-routerAuth.patch("/users", authenticate, updateUser)
+routerAuth.delete("/user/:id/admin", authenticate, authorization, removebyAdmin);
+routerAuth.delete("/users/:id", authenticate, authorization, removebyUser);
+routerAuth.patch("/user/:id/admin", authenticate, authorization, updateUserByAdmin);
+routerAuth.patch("/users/:id", authenticate, authorization, updateUser);
 routerAuth.post("/signup", signup);
 routerAuth.post("/signin", signin);
 routerAuth.post("/logout", authenticate, logout);
