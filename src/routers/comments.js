a01cd2005146f2b7a53eccbc3ae956ac70b23597
Tran Hai Duong <haiduong12122003@gmail.com>
2023-09-24 21:07:13 +0700

    feat : Admin can delete comment user

--- src/routers/comments.js
+++ src/routers/comments.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { create, getAllComment, getCommentFromProduct, getOneComment, removeCommentByUser, updateComment } from "../controllers/comments.js";
+import { create, getAllComment, getCommentFromProduct, getOneComment, removeCommentByAdmin, removeCommentByUser, updateComment } from "../controllers/comments.js";
 import { authorization } from "../middlewares/authorization.js";
 import { authenticate } from "../middlewares/authenticate.js";
 
@@ -9,7 +9,7 @@ routerComment.get("/comment/:productId", getCommentFromProduct);
 routerComment.get("/comment/:id/detail", getOneComment);
 routerComment.get("/comment", getAllComment)
 routerComment.delete("/comments/:id/remove", removeCommentByUser);
-// routerComment.delete("/comment/:id/admin", authenticate, authorization, removeCommentByAdmin);
+routerComment.delete("/comment/:id/admin", authenticate, authorization, removeCommentByAdmin);
 routerComment.post("/comment", create);
 routerComment.patch("/comment/:id", updateComment);
 
