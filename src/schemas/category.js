2481760c44ab2c2bd1411844bea8a230c939d4e4
Minh452003 <117347792+Minh452003@users.noreply.github.com>
2023-10-20 17:57:37 +0700

    fix: upload image

--- src/schemas/category.js
+++ src/schemas/category.js
@@ -1,6 +1,7 @@
 import Joi from "joi"
 
 export const categorySchema = Joi.object({
+    _id: Joi.string(),
     category_name: Joi.string().required().messages({
         "string.empty": "Tên danh mục bắt buộc nhập",
         "any.required": "Trường danh mục bắt buộc nhập"
