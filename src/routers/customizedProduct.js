25bc2ca237bfaca5805528075b4d943da8720465
Your Name <chubachduong582003@gmail.com>
2023-11-07 21:05:57 +0700

    fix:Custom Products

--- src/routers/customizedProduct.js
+++ src/routers/customizedProduct.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { createCustomizedProduct, get, getAllCustomProduct, getAllDelete, listCustomizedProducts, remove, removeForce, restoreProduct, updateProduct } from "../controllers/customizedProduct.js";
+import { createCustomizedProduct, get, getAllCustomProduct, getAllDelete, getAllDeleteById, listCustomizedProducts, remove, removeForce, restoreProduct, updateProduct } from "../controllers/customizedProduct.js";
 const routerCustomizedProduct = express.Router();
 
 routerCustomizedProduct.get("/customized-products-list", getAllCustomProduct);
-routerCustomizedProduct.get("/CustomProducts/delete/:userId", getAllDelete)
+routerCustomizedProduct.get("/CustomProducts/delete/:userId", getAllDeleteById)
 routerCustomizedProduct.get("/customized-products/:userId", listCustomizedProducts);
-routerCustomizedProduct.get("/customized-products/delete/:userId", getAllDelete);
+routerCustomizedProduct.get("/CustomProducts/delete", getAllDelete);
 routerCustomizedProduct.get("/customized-products/id/:id", get);
 routerCustomizedProduct.delete("/customized-products/:id", remove);
 routerCustomizedProduct.delete("/customized-products/force/:id", removeForce);
