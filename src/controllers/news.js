8504f8baf4d30e13c6786ca2756890dc4eaaca87
Duyy2707 <duy27072003@gmail.com>
2023-11-07 20:33:41 +0700

    fix: create news

--- src/controllers/news.js
+++ src/controllers/news.js
@@ -2,7 +2,7 @@ import New from "../models/news.js";
 import { newSchema } from "../schemas/news.js";
 
 
-export const getNewList = async (req, res) => {
+export const getNews = async (req, res) => {
     try {
         const news = await New.find();
         if (news.length === 0) {
@@ -21,8 +21,72 @@ export const getNewList = async (req, res) => {
     }
 };
 
+export const getAllDelete = async (req, res) => {
+    try {
+      const news = await New.findWithDeleted({ deleted: true });
+      return res.status(200).json({
+        message: "Lấy tất cả tin tức đã bị xóa",
+        news
+      });
+    } catch (error) {
+      return res.status(400).json({
+        message: error,
+      })
+    }
+  };
 
-export const createNews = async (req, res) => {
+  export const getNewById = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const news = await New.findById(id);
+        if (news.length === 0) {
+            return res.status(400).json({
+                message: "Không có dữ liệu!",
+            })
+        }
+        return res.status(200).json({
+            message: "Lấy 1 tin tức thành công",
+            news
+        });
+    } catch (error) {
+        return res.status(400).json({
+            message: error,
+        })
+    }
+};
+
+
+export const removeNew = async (req, res) => {
+    try {
+      const id = req.params.id;
+      const news = await New.deleteById(id);
+      return res.status(200).json({
+        message: "Xoá tin tức thành công.!",
+        news
+      });
+    } catch (error) {
+      return res.status(400).json({
+        message: error,
+      });
+    }
+  };
+
+export const removeForceNew = async (req, res) => {
+    try {
+      const news = await New.deleteOne({ _id: req.params.id });
+      return res.status(200).json({
+        message: "Xoá sản phẩm vĩnh viễn",
+        news
+      })
+    } catch (error) {
+      return res.status(400).json({
+        message: error,
+      })
+    }
+  };
+
+
+export const addNew = async (req, res) => {
     try {
         const { error } = newSchema.validate(req.body);
         if (error) {
@@ -48,66 +112,60 @@ export const createNews = async (req, res) => {
 };
 
 
-export const updateNews = async (req, res) => {
+
+
+export const restoreNew = async (req, res) => {
     try {
-        const id = req.params.id;
-        const body = req.body;
-        const { error } = newSchema.validate(body, { abortEarly: false });
-        if (error) {
-            const errors = error.details.map((err) => err.message)
-            return res.status(400).json({
-                message: errors
-            })
-        }
-        const news = await New.findByIdAndUpdate(id, body, { new: true, });
-        if (!news) {
-            return res.status(404).json({
-                message: 'Cập nhật tin tức thất bại',
-            });
-        }
-        return res.status(200).json({
-            message: "Cập nhật tin tức thành công",
-            news
-        })
-    } catch (error) {
+      const restoredNew = await New.restore({ _id: req.params.id }, { new: true });
+      if (!restoredNew) {
         return res.status(400).json({
-            message: error.message
-        })
+          message: "Tin tức không tồn tại hoặc đã được khôi phục trước đó.",
+        });
+      }
+  
+      return res.status(200).json({
+        message: "Khôi phục tin tức thành công.",
+        news: restoredNew,
+      });
+    } catch (error) {
+      return res.status(400).json({
+        message: error.message,
+      });
     }
+  };
+  
+
+
+export const updateNew = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const body = req.body;
+    const { error } = newSchema.validate(body, { abortEarly: false });
+    if (error) {
+      const errors = error.details.map((err) => err.message);
+      return res.status(400).json({
+        message: errors,
+      });
+    }
+    const news = await New.findOneAndUpdate({ _id: id }, body, {
+      new: true,
+    });
+    if (!news || news.length === 0) {
+      return res.status(400).json({
+        message: "Cập nhật tin tức thất bại",
+      });
+    }
+    return res.status(200).json({
+      message: "Cập nhật tin tức thành công",
+      news,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      message: error.message,
+    });
+  }
 };
 
 
-export const removeNews = async (req, res) => {
-    try {
-        const news = await New.findByIdAndDelete(req.params.id);
-        return res.status(200).json({
-            message: 'Xóa tin tức thành công',
-            news
-        });
-    } catch (error) {
-        return res.status(400).json({
-            message: error,
-        });
-    }
-};
 
 
-export const getNewByOneId = async (req, res) => {
-    try {
-        const id = req.params.id;
-        const news = await New.findById(id);
-        if (news.length === 0) {
-            return res.status(400).json({
-                message: "Không có dữ liệu!",
-            })
-        }
-        return res.status(200).json({
-            message: "Lấy 1 tin tức thành công",
-            news
-        });
-    } catch (error) {
-        return res.status(400).json({
-            message: error,
-        })
-    }
-};
--- src/routers/news.js
+++ src/routers/news.js
@@ -1,11 +1,17 @@
 import express from 'express';
-import { createNews, getNewByOneId, getNewList, removeNews, updateNews } from "../controllers/news.js";
+import { addNew, getAllDelete, getNewById, getNews, removeForceNew, removeNew, restoreNew, updateNew } from "../controllers/news.js";
+import { authorization } from '../middlewares/authorization.js';
+import { authenticate } from '../middlewares/authenticate.js';
 
 const routerNews = express.Router();
 
-routerNews.post('/news', createNews);
-routerNews.get('/news', getNewList);
-routerNews.patch("/news/:id", updateNews);
-routerNews.get("/news/:id", getNewByOneId);
-routerNews.delete("/news/:id", removeNews);
+
+routerNews.get("/news", getNews);
+routerNews.get("/news/delete", getAllDelete);
+routerNews.get("/news/:id", getNewById);
+routerNews.delete("/news/:id", authenticate, authorization, removeNew);
+routerNews.delete("/news/force/:id", authenticate, authorization, removeForceNew);
+routerNews.post("/news", authenticate, authorization, addNew);
+routerNews.patch("/news/:id", authenticate, authorization, updateNew);
+routerNews.patch("/news/restore/:id", authenticate, authorization, restoreNew);
 export default routerNews;
