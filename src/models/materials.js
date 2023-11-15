4d5b96f5f2a3cc62fbd83d212103539a85ba7a6f
nguyenloc9999 <locnvph27057@fpt.edu.vn>
2023-11-15 22:18:13 +0700

    fix_sizes_and_materials

--- src/models/materials.js
+++ src/models/materials.js
@@ -4,10 +4,6 @@ const materialSchema = mongoose.Schema({
     material_name: {
         type: String,
         required: true
-    },
-    material_price: {
-        type: Number,
-        required: true
     }
 },
     { timestamps: true, versionKey: false });
