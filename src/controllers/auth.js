b48e61f1efeb90552ac756adaa1b8b58eaf78873
Your Name <chubachduong582003@gmail.com>
2023-12-08 00:07:23 +0700

    fix: message and update user

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -137,13 +137,7 @@ export const updateUserByAdmin = async (req, res) => {
     try {
         const id = req.params.id;
         const body = req.body;
-        const { error } = updateUserSchema.validate(body, { abortEarly: false });
-        if (error) {
-            const errors = error.details.map((err) => err.message);
-            return res.status(400).json({
-                message: errors,
-            });
-        }
+
         const user = await Auth.findByIdAndUpdate(id, body, { new: true }).select(
             "-password -role -refreshToken -passwordChangeAt -__v"
         );
